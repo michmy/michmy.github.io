9284065d3636e755fffc9a5a90fe977e77ef867c
Michelle <michmy@gmail.com>
2017-03-28 10:09:56 -0400

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,68 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+const extractLess = new ExtractTextPlugin({
+    filename: "default.css"
+});
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './assets/js/src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
- 
   resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
+      modules: [
+       path.join(__dirname, "src"),
+       "node_modules"
+     ]
   },
-  plugins: [
-  new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery"
-  })
-  ],
   module: {
-      loaders: [
-            {
-            test: /jquery/,
-            loader: 'expose?$!expose?jQuery'
-            },
-            {
-            test: /\.vue$/,
-            loader: 'vue'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015']
-                },
-                exclude: /node_modules/
-            },
-            {
+      rules:
+          [{
+            test: /\.less$/,
+            use: extractLess.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        },
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        },
+        {
             test: /\.(png|jpg|gif|svg)$/,
-            loader: 'file',
-            query: {
+            use:
+            {
+                loader: 'file-loader',
+                options: {
                     name: '[name].[ext]?[hash]'
                 }
             }
-    ]
+        }]
   },
+  plugins: [
+      extractLess
+  ],
   devServer: {
     historyApiFallback: true,
     noInfo: true
   },
-  devtool: '#eval-source-map'
+  devtool: 'eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = 'source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
