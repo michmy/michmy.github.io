cd6bc55e23f2a5488c98586dde40c21029f64800
Michelle Yang <michmy@gmail.com>
2024-05-03 19:11:47 -0400

    Use 11ty and tailwind

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+	
+	eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+	
+	eleventyConfig.addPassthroughCopy('./src/images/');
+	eleventyConfig.addPassthroughCopy({ './src/favicons': '/' });
+	eleventyConfig.addPassthroughCopy("./src/styles/default.css");
+
+	return {
+		dir: {
+			input: 'src',
+			includes: '_includes',
+		},
+	};
+};
